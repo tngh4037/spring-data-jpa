e86eacdeec0dfa0dfee03733bb9526f2122f68c7
tngh4037 <tngh4037@gmail.com>
2024-06-02 01:20:57 +0900

    section3: 공통 인터페이스 기능 - 순수 JPA 기반 리포지토리 만들기

--- src/main/java/study/data_jpa/repository/MemberJpaRepository.java
+++ src/main/java/study/data_jpa/repository/MemberJpaRepository.java
@@ -5,6 +5,9 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import study.data_jpa.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class MemberJpaRepository {
 
@@ -14,6 +17,25 @@ public class MemberJpaRepository {
     public Member save(Member member) {
         em.persist(member);
         return member;
+    }
+
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList(); // 리스트 반환
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class) // 참고) JPA에서 count()는 Long 타입을 return 함
+                .getSingleResult(); // 단건 반환
     }
 
     public Member find(Long id) {
---
+++ src/main/java/study/data_jpa/repository/TeamRepository.java
@@ -0,0 +1,44 @@
+package study.data_jpa.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import study.data_jpa.entity.Team;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class TeamRepository {
+
+    @PersistenceContext // JPA entityManager 를 주입해주는 애노테이션
+    private EntityManager em;
+
+    public Team save(Team team) {
+        em.persist(team);
+        return team;
+    }
+
+    public void delete(Team team) {
+        em.remove(team);
+    }
+
+    public List<Team> findAll() {
+        return em.createQuery("select t from Team t", Team.class)
+                .getResultList();
+    }
+
+    public Optional<Team> findById(Long id) {
+        Team team = em.find(Team.class, id);
+        return Optional.ofNullable(team);
+    }
+
+    public Team find(Long id) {
+        return em.find(Team.class, id);
+    }
+
+    public long count() {
+        return em.createQuery("select count(t) from Team t", Long.class)
+                .getSingleResult();
+    }
+}
