635a1140c00acabaf8fb9beadefc9e938e5538bb
tngh4037 <tngh4037@gmail.com>
2024-06-02 00:30:29 +0900

    section2: 예제 도메인 모델

--- src/main/java/study/data_jpa/entity/Member.java
+++ src/main/java/study/data_jpa/entity/Member.java
@@ -1,23 +1,40 @@
 package study.data_jpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // = protected Member() {}
+@ToString(of = {"id", "username", "age"}) // team 은 적으면 안된다. 서로 참조하고 있기 때문에 무한루프가 걸릴 수 있다. 따라서 가급적이면 연관관계 필드는 toString 에서 제외하는 게 좋다.
 public class Member {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
-    protected Member() {
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
